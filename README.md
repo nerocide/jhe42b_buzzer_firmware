b613a1cad5865b8620c1ce59437e7fdd16b5f04e
nerocide <jean.mich.c@gmail.com>
2024-04-22 06:52:58 +0200

    commit relics found

--- README.md
+++ README.md
@@ -15,7 +15,6 @@ Espacially disarming buzzer without using onboard button.
 - [ ] Power saving by changing BEEPS frequency 10s / 30s / 60s (default 30s)
 - [x] LIGHT flashs for every beeps (built-in)
 
-
 # How to flash
 ## Requirements
 - [ST-link v2](https://a.aliexpress.com/_ExNkgOT) (so you can write firmware onto the STM8 flash memory) [how to connect it?](#STMLinker-connection)
@@ -43,7 +42,6 @@ TODO
 Otherwise add board definition [stm8s003f3.json](https://github.com/nerocide/platform-ststm8/blob/develop/boards/stm8s003f3.json) in :
 > C:\Users\<user>\.platformio\platforms\ststm8\boards
 
-
 ## Unlock chip
 Every chip has to be unlocked once. (from experience)
 ```
@@ -57,13 +55,11 @@ You should connect your wires as follow to the linker.
 
 <img src="https://user-images.githubusercontent.com/32848699/128609610-dc31b74a-f0d0-4a57-a1cb-39078045d49f.jpg" width="500"/>
 
-
 If you don't want to solder on board, you can use a contraption like so...
 
 <img src="https://www.creavea.com/produits/14928-l/pince-a-linge-geante-en-bois-l.jpg" width="100"/>
 <img src="https://proofmart.com/wp-content/uploads/2021/06/541-34-2048x2048.png" width="50"/>
 <img src="https://www.scondar.com/wp-content/uploads/2017/03/1mm-vertical-pin-header.jpg" width="135"/>
-
 
 # Compatible Boards
 Model|Controller|Chip Mark|Pictures
@@ -71,32 +67,17 @@ Model|Controller|Chip Mark|Pictures
 JHE42B|STM8S003F3 TSSOP20|STM8S003F3P6|<img src="ressources/img/JHE42B_front.jpg" width="250"/> <img src="ressources/img/JHE42B_back.jpg" width="250"/>
 JHE42B_S|STM8S003F3 UFQFPN20|S033PHVG822Y|<img src="ressources/img/JHE42B_S_front.jpg" width="250"/> <img src="ressources/img/JHE42B_S_back.jpg" width="250"/>
 
-
 ## Commercial shot
-<!-- ![Commercial shot](/img/both_buzzer_full.jpg =100x) -->
 <img src="ressources/img/both_buzzer_full.jpg" width="300"/>
-
-
-<!-- ## JHE42B -->
-<!-- ![JHE42B front](/img/JHE42B_front.jpg) -->
-<!-- ![JHE42B back](/img/JHE42B_back.jpg) -->
-
-<!-- <img src="ressources/img/JHE42B_front.jpg" width="300"/> <img src="ressources/img/JHE42B_back.jpg" width="300"/> -->
-<!-- <img src="/img/jhe42b_schematic.png" width="600"/> -->
-
-<!-- ## JHE42B_S -->
-<!-- ![JHE42B_S front](/img/JHE42B_S_front.jpg) -->
-<!-- ![JHE42B_S back](/img/JHE42B_S_back.jpg) -->
-
-<!-- <img src="ressources/img/JHE42B_S_front.jpg" width="300"/> <img src="ressources/img/JHE42B_S_back.jpg" width="300"/> -->
 
 # Reverse process
 (checked if working)
 
 - [x] power supply detection
-- [x] blink onboard LED
-- [x] blink onboard LED
-- [x] bip on bipper
+- [x] blink onboard red LED
+- [ ] blink onboard white flash LED
+- [x] sound on buzzer
 - [x] button press
+
 # Resources
 [ST STM8S003F3 DataSheet](ressources/doc/STM8S0003F3_datasheet_dm00024550.pdf)
--- src/main.c
+++ src/main.c
@@ -1,19 +1,20 @@
 #include <Arduino.h>
 #include <notes.h>
-// const
+// board pinout
 #define powerPin 1  // PA2
 #define buttonPin 2 // PA3
-// #define ledBuzzerPin 5 // not used?
+// #define ledBuzzerPin 5 //not used?
 #define redLedPin 8 // PC6
 #define buzzerPin 13  // PD4
 #define signalPin 7 // PC5
-
+// const
 const int disarmDelay=10; // in seconds, how long should power be applied to disarm
-const int shortPressDelay=100; // in miliseconds, how long to press the button to register a press
-const int longPressDelay=500; // in miliseconds, how long to press the button to register a press
+const int shortPressDelay=200; // in miliseconds, how long to press the button to register a press
+const int longPressDelay=600; // in miliseconds, how long to press the button to register a press
 const int graceDelay=50; // in miliseconds, how long to press the button to register a press
 bool armed = FALSE;
 bool powerOutage = FALSE;
+bool buttonPressed = FALSE;
 int alarmInterval = 30; // number of seconds between each beeps
 int shortPressCount=0; // register number of time the bouton is short pressed
 int longPressCount=0; // register number of time the bouton is long pressed
@@ -26,6 +27,7 @@ void setup()  { // setup pins I/O
   pinMode(powerPin,INPUT);
   pinMode(signalPin,INPUT);
   pinMode(redLedPin,OUTPUT);
+  // pinMode(ledBuzzerPin,OUTPUT);
   pinMode(buzzerPin,OUTPUT);
 }
 
@@ -36,72 +38,83 @@ void disarm(){
   play_disarming(buzzerPin); // play disarm sound
 }
 
+void arm(){
+  armed = TRUE; // arm the system
+  digitalWrite(redLedPin, HIGH); // turn on red led onboard      
+  play_arming(buzzerPin); // play arming sound
+}
+
 void loop() {
   int mainPowerState = digitalRead(powerPin); // HIGH when plugged
   int buttonState = digitalRead(buttonPin); //LOW when pressed
   int signalState = digitalRead(signalPin); //LOW when activated
   unsigned long currentMillis=millis(); // current time marker
-  
 
   if (mainPowerState == HIGH){ // when main power connected
-    if(pluggedMillis == 0){ // and no plugged timestamp
-      pluggedMillis=millis(); // store plugged timestamp
-      powerCutMillis=0; // reset power loss timestamp
-    }
+    // if (pluggedMillis == 0){ // and no plugged timestamp
+    //   pluggedMillis=millis(); // store plugged timestamp
+    //   powerCutMillis=0; // reset power loss timestamp
+    // }
     if (armed == FALSE){ // if not armed, arm buzzer
-      armed = TRUE; // arm the system
-      digitalWrite(redLedPin, HIGH); // turn on red led onboard      
-      play_arming(buzzerPin); // play arming sound
+      pluggedMillis=millis();
+      arm();
     }
     if (powerOutage == TRUE){ //if there is an outage
       pluggedMillis=millis(); // store plugged timestamp
       powerCutMillis = 0; // reset power cut timestamp so we can detect outage
+      powerOutage = FALSE; // register power outage
     }
   }else{ // when main power is disconnected
     if (armed == TRUE){ // if buzzer is armed
       if (powerOutage == FALSE){ // if no outage is present
         powerCutMillis=millis(); // store power cut timestamp
-        powerOutage = TRUE; // register power outage
+        powerOutage=TRUE; // register power outage
       }
       if (round((currentMillis - powerCutMillis)/1000) >= alarmInterval){ // when power outage duration is equal or more than alarmInterval
-        pluggedMillis=0; // reset plugged timestamp
+        powerCutMillis=millis(); // store current time for next alarm
         play_alarm(buzzerPin); // play alarm
       }
       if (powerOutage == TRUE){ // if there is an outage
         if ((round((currentMillis - pluggedMillis)/1000) > (disarmDelay-1)) && (round((currentMillis - pluggedMillis)/1000) < (disarmDelay+1))){ // and power was applied for disarmDelay ~ +/- 1s
           disarm();
         }
-        if (shortPressCount > 1){ // if the button is pressed disarm the system
+        if (longPressCount > 1){ // if the button is pressed disarm the system
+          longPressCount=0;
           disarm();
-          shortPressCount=0;
         }
       }
     }
   }
   // SIGNALBUZZER
-  if (signalState == HIGH){ // when signal is enable play sound
-    digitalWrite(redLedPin, HIGH);
+  if (signalState == LOW){ // when signal is enable play sound
     play_alarm(buzzerPin);
-  }else{
-    digitalWrite(redLedPin, LOW);
   }
   // BUTTON MANAGEMENT
   if (buttonState == LOW) {
     digitalWrite(redLedPin, HIGH);
-    if(buttonPressMillis == 0){
+    // if(buttonPressMillis == 0){
+    //   buttonPressMillis=millis();
+    // }
+    if (buttonPressed == FALSE)
+    {
+      buttonPressed=TRUE;
       buttonPressMillis=millis();
     }
   }else{
     digitalWrite(redLedPin, LOW);
-    // detect short press
-    if ((round(currentMillis - buttonPressMillis) > shortPressDelay-graceDelay) && (round(currentMillis - buttonPressMillis) < shortPressDelay+graceDelay)){
-      shortPressCount=shortPressCount+1;
-      buttonPressMillis=0;
-    }
-    // detect short press
-    if ((round(currentMillis - buttonPressMillis) > longPressDelay-graceDelay) && (round(currentMillis - buttonPressMillis) < longPressDelay+graceDelay)){
-      longPressCount=longPressCount+1;
-      buttonPressMillis=0;
+    if (buttonPressed == TRUE)
+    {
+      buttonPressed=FALSE;
+      // detect short press
+      if ((round(currentMillis - buttonPressMillis) > shortPressDelay-graceDelay) && (round(currentMillis - buttonPressMillis) < shortPressDelay+graceDelay)){
+        shortPressCount=shortPressCount+1;
+        buttonPressMillis=0;
+      }
+      // detect short press
+      if ((round(currentMillis - buttonPressMillis) > longPressDelay-graceDelay) && (round(currentMillis - buttonPressMillis) < longPressDelay+graceDelay)){
+        longPressCount=longPressCount+1;
+        buttonPressMillis=0;
+      }
     }
   }
 }
