8ccd1be2ec83f970f3c9ccf6625b93d90c621c7d
nerocide <nerocide@hotmail.com>
2024-07-07 04:38:14 +0200

    feature tested for JHE42B

--- src/main.c
+++ src/main.c
@@ -10,9 +10,10 @@
 // const
 const int disarmDelay=10; // in seconds, how long should power be applied to disarm
 const int shortPressDelay=200; // in miliseconds, how long to press the button to register a press
-const int longPressDelay=600; // in miliseconds, how long to press the button to register a press
-const int graceDelay=50; // in miliseconds, how long to press the button to register a press
+const int longPressDelay=2000; // in miliseconds, how long to press the button to register a press
+const int graceDelay=500; // in miliseconds, how long to press the button to register a press
 bool armed = FALSE;
+bool disarmed = FALSE;
 bool powerOutage = FALSE;
 bool buttonPressed = FALSE;
 int alarmInterval = 30; // number of seconds between each beeps
@@ -21,6 +22,7 @@ int longPressCount=0; // register number of time the bouton is long pressed
 unsigned long powerCutMillis=0;
 unsigned long pluggedMillis=0; // store plugged timestamp
 unsigned long buttonPressMillis=0; // store button press timestamp
+unsigned long lastAlarmMillis=0; // store previous alarm timestamp
 
 void setup()  { // setup pins I/O
   pinMode(buttonPin,INPUT);
@@ -32,6 +34,7 @@ void setup()  { // setup pins I/O
 }
 
 void disarm(){
+  disarmed = TRUE; // disarm the system
   armed = FALSE; // disarm the system
   powerCutMillis = 0; // reset power cut timestamp so we can detect outage
   digitalWrite(redLedPin, LOW); // turn off red led onboard
@@ -42,6 +45,8 @@ void arm(){
   armed = TRUE; // arm the system
   digitalWrite(redLedPin, HIGH); // turn on red led onboard      
   play_arming(buzzerPin); // play arming sound
+  shortPressCount=0;
+  longPressCount=0;
 }
 
 void loop() {
@@ -51,11 +56,7 @@ void loop() {
   unsigned long currentMillis=millis(); // current time marker
 
   if (mainPowerState == HIGH){ // when main power connected
-    // if (pluggedMillis == 0){ // and no plugged timestamp
-    //   pluggedMillis=millis(); // store plugged timestamp
-    //   powerCutMillis=0; // reset power loss timestamp
-    // }
-    if (armed == FALSE){ // if not armed, arm buzzer
+    if ((armed == FALSE) && (disarmed == FALSE)){ // if not armed, arm buzzer
       pluggedMillis=millis();
       arm();
     }
@@ -64,44 +65,49 @@ void loop() {
       powerCutMillis = 0; // reset power cut timestamp so we can detect outage
       powerOutage = FALSE; // register power outage
     }
-  }else{ // when main power is disconnected
+  }
+  // when main power is disconnected
+  if (mainPowerState == LOW){ // when main power connected
     if (armed == TRUE){ // if buzzer is armed
       if (powerOutage == FALSE){ // if no outage is present
         powerCutMillis=millis(); // store power cut timestamp
         powerOutage=TRUE; // register power outage
+        lastAlarmMillis=powerCutMillis;
       }
-      if (round((currentMillis - powerCutMillis)/1000) >= alarmInterval){ // when power outage duration is equal or more than alarmInterval
-        powerCutMillis=millis(); // store current time for next alarm
+      
+      if (round((currentMillis - lastAlarmMillis)/1000) >= alarmInterval){ // when power outage duration is equal or more than alarmInterval
+        lastAlarmMillis=millis(); // store current time for next alarm
         play_alarm(buzzerPin); // play alarm
       }
       if (powerOutage == TRUE){ // if there is an outage
-        if ((round((currentMillis - pluggedMillis)/1000) > (disarmDelay-1)) && (round((currentMillis - pluggedMillis)/1000) < (disarmDelay+1))){ // and power was applied for disarmDelay ~ +/- 1s
-          disarm();
-        }
-        if (longPressCount > 1){ // if the button is pressed disarm the system
-          longPressCount=0;
+        if ((round((powerCutMillis - pluggedMillis)/1000) > (disarmDelay-1)) && (round((powerCutMillis - pluggedMillis)/1000) < (disarmDelay+1))){ // and power was applied for disarmDelay ~ +/- 1s
           disarm();
         }
       }
+    }
+    if (disarmed == TRUE){
+      armed = FALSE;
+      disarmed = FALSE;
     }
   }
   // SIGNALBUZZER
   if (signalState == LOW){ // when signal is enable play sound
     play_alarm(buzzerPin);
   }
+  // Disarm with button
+  if ((armed == TRUE) && (longPressCount > 0)){ // if the button is pressed disarm the system
+    longPressCount=0;
+    disarm();
+  }
+
   // BUTTON MANAGEMENT
   if (buttonState == LOW) {
-    digitalWrite(redLedPin, HIGH);
-    // if(buttonPressMillis == 0){
-    //   buttonPressMillis=millis();
-    // }
     if (buttonPressed == FALSE)
     {
       buttonPressed=TRUE;
       buttonPressMillis=millis();
     }
   }else{
-    digitalWrite(redLedPin, LOW);
     if (buttonPressed == TRUE)
     {
       buttonPressed=FALSE;
@@ -111,7 +117,8 @@ void loop() {
         buttonPressMillis=0;
       }
       // detect short press
-      if ((round(currentMillis - buttonPressMillis) > longPressDelay-graceDelay) && (round(currentMillis - buttonPressMillis) < longPressDelay+graceDelay)){
+      // if ((round(currentMillis - buttonPressMillis) > longPressDelay-graceDelay) && (round(currentMillis - buttonPressMillis) < longPressDelay+graceDelay)){
+      if ((round(currentMillis - buttonPressMillis) > longPressDelay)){
         longPressCount=longPressCount+1;
         buttonPressMillis=0;
       }
